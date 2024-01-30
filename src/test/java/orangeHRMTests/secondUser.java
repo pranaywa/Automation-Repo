cf02e1863ebc8836a0e4d3f7bc167945ab8be47e
pranaywane@gmail.com <157872831+pranaywa@users.noreply.github.com>
2024-01-30 12:48:45 +0530

    second user update is added in repo

---
+++ src/test/java/orangeHRMTests/secondUser.java
@@ -0,0 +1,20 @@
+package orangeHRMTests;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+import orangeHRMPages.AdminUserManagementPage;
+import orangeHRMPages.OrangeHRMLogin;
+import utility.ReadPropData;
+import utility.TimeGen;
+
+public class secondUser {
+public void userTest(){
+System.out.println("this is a user second update ");
+}
+
+}
